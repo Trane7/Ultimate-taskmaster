62afa815ede8209240d8bd0cd64aaca644d6d206
Dee <dgrowette@gmail.com>
2021-10-25 12:52:40 -0700

    added edit textbox of task

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,8 +44,15 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-
-
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+  .text()
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  $(this).replaceWith(textInput)
+  textInput.trigger("focus")
+});  
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
